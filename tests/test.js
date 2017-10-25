538d0ea103d96b5a2ef008a18cecbf8a000be28c
Vendredix <3677649+Vendredix@users.noreply.github.com>
2017-10-25 23:26:18 +0200

    Implemented Promise.prototype.finally

--- tests/test.js
+++ tests/test.js
@@ -3,9 +3,9 @@ require("../lib/index");
 
 const assert = require('assert');
 
-describe('Promise', function() {
-  describe('.delay(1000)', function() {
-    it('should resolve after 1000 milliseconds', async function() {
+describe('Promise', function () {
+  describe('.delay(1000)', function () {
+    it('should resolve after 1000 milliseconds', async function () {
       const start = Date.now();
       const result = await Promise.delay(1000);
 
@@ -15,14 +15,14 @@ describe('Promise', function() {
   });
 
 
-  describe('#return()', function() {
-    it('should resolve with given value', async function() {
+  describe('#return()', function () {
+    it('should resolve with given value', async function () {
       const promise = Promise.resolve("test");
 
       assert.equal("test", await promise);
       assert.equal("returned", await promise.return("returned"));
     });
-    it('should resolve with given value (with thenReturn)', async function() {
+    it('should resolve with given value (with thenReturn)', async function () {
       const promise = Promise.resolve("test");
 
       assert.equal("test", await promise);
@@ -30,8 +30,8 @@ describe('Promise', function() {
     });
   });
 
-  describe('#throw()', function() {
-    it('should reject with given reason', async function() {
+  describe('#throw()', function () {
+    it('should reject with given reason', async function () {
       const promise = Promise.resolve("test");
 
       assert.equal("test", await promise);
@@ -43,7 +43,7 @@ describe('Promise', function() {
         assert.equal(throwErr, err, "error should be equal");
       }
     });
-    it('should reject with given reason (with thenThrow)', async function() {
+    it('should reject with given reason (with thenThrow)', async function () {
       const promise = Promise.resolve("test");
 
       assert.equal("test", await promise);
@@ -53,6 +53,40 @@ describe('Promise', function() {
         assert.notEqual(true, true, "promise should throw error");
       } catch (err) {
         assert.equal(throwErr, err, "error should be equal");
+      }
+    });
+  });
+
+
+  describe('#finally()', function () {
+    it('should be called if resolved', async function () {
+      let called = false;
+      const result = await Promise.resolve("ORG_RESULT")
+        .finally(() => {
+          called = true;
+          return "FINALLY_RESULT";
+        });
+
+      assert.equal(true, called, "finally should have been called");
+      assert.equal("ORG_RESULT", result, "result should be equal");
+    });
+
+    it('should be called if rejected', async function () {
+      let called = false;
+      const throwErr = new Error("SOME_ERROR");
+      let result;
+
+      try {
+        result = await Promise.resolve("ORG_RESULT")
+          .throw(throwErr)
+          .finally(() => {
+            called = true;
+            return "FINALLY_RESULT";
+          });
+      } catch (err) {
+        assert.equal(true, called, "finally should have been called");
+        assert.equal(throwErr, err, "error should be equal");
+        assert.equal(void 0, result, "result should be undefined");
       }
     });
   });
