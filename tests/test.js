7a6003cde60d12c4c1cbed083b1f5a2c53cfe41a
Vendredix <3677649+Vendredix@users.noreply.github.com>
2017-10-26 20:45:11 +0200

    Updated Promise.each

--- tests/test.js
+++ tests/test.js
@@ -144,4 +144,62 @@ describe('Promise', function () {
       return promise;
     });
   });
+
+
+  describe('#each()', function() {
+    it('should be executed for each entry', function() {
+      return testEach((items, iterator) => Promise.resolve(items).each(iterator))
+    });
+
+    // TODO: test rejection
+  });
+  describe('.each()', function() {
+    it('should be executed for each entry', function() {
+      return testEach((items, iterator) => Promise.each(items, iterator));
+    });
+  });
 });
+
+async function testEach(eachHandler) {
+  function Constructor(nr) {
+    this.nr = nr;
+    this.called = false;
+  }
+
+  const items = [];
+  for (let i = 0; i < 42; i++) {
+    let val = new Constructor(i);
+    if (i % 4 > 0) val = Promise.resolve(val);
+    if (i % 4 > 1) val = Promise.resolve(val);
+    if (i % 4 > 2) val = Promise.resolve(val);
+    items.push(val);
+  }
+
+  let index = 0
+    , isRunning = false;
+
+  const result = await eachHandler(items, (item, i, len) => {
+    item.called = true;
+
+    assert.strictEqual(isRunning, false, "should have awaited promise serially");
+    assert.strictEqual(len, items.length, "length should be equal");
+    assert.strictEqual(i, index++, "index should be equal");
+    assert.strictEqual(item instanceof Constructor, true, "item should be resolved and an instance of Constructor");
+
+    isRunning = true;
+
+    return Promise.resolve()
+      .then(() => {
+        isRunning = false;
+        return "SOME_VALUE" + i;
+      });
+  });
+
+  assert.strictEqual(index, items.length, "should have handled each item");
+  assert.strictEqual(isRunning, false, "should have handled each item");
+  assert.strictEqual(result, items, "should return with original array");
+
+  items.forEach((item, i) => {
+    assert.strictEqual(item, items[i], "should return with original array item" + i);
+  });
+}
