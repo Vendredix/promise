bd7adae91443d69c5e02674995fe36868b736066
Vendredix <3677649+Vendredix@users.noreply.github.com>
2019-04-25 21:55:29 +0200

    Fixed reduce not being used for the first entry if no intial value is provided

--- tests/test-reduce.js
+++ tests/test-reduce.js
@@ -7,5 +7,72 @@ describe('promise-reduce', function () {
 
 
   // TODO: test reduce
+
+  describe('.reduce()', function () {
+    it('should sum without initial value', async function () {
+      let called = 0;
+      const total = await Promise.reduce([
+        Promise.resolve(1),
+        Promise.resolve(2),
+        Promise.resolve(3),
+      ], (acc, value) => {
+        called++;
+        return (acc || 0) + value;
+      });
+
+      assert.strictEqual(total, 1+2+3, "Sum does not match");
+      assert.strictEqual(called, 3, "Reducer method must be used");
+    });
+
+    it('should concat without initial value', async function () {
+      let called = 0;
+      const total = await Promise.reduce([
+        Promise.resolve(1),
+        Promise.resolve(2),
+        Promise.resolve(3),
+      ], (acc, value) => {
+        called++;
+
+        if (!acc) return [value];
+        return acc.concat(value);
+      });
+
+      assert.deepStrictEqual(total, [1,2,3], "Sum does not match");
+      assert.strictEqual(called, 3, "Reducer method must be used");
+    });
+
+
+    it('should sum with initial value', async function () {
+      let called = 0;
+      const total = await Promise.reduce([
+        Promise.resolve(1),
+        Promise.resolve(2),
+        Promise.resolve(3),
+      ], (acc, value) => {
+        called++;
+        return acc + value;
+      }, 10);
+
+      assert.strictEqual(total, 10+1+2+3, "Sum does not match");
+      assert.strictEqual(called, 3, "Reducer method must be used");
+    });
+
+    it('should concat with initial value', async function () {
+      let called = 0;
+      const total = await Promise.reduce([
+        Promise.resolve(1),
+        Promise.resolve(2),
+        Promise.resolve(3),
+      ], (acc, value) => {
+        called++;
+        assert.strictEqual(Array.isArray(acc), true, "Accumulator should be an array");
+
+        return acc.concat(value);
+      }, []);
+
+      assert.deepStrictEqual(total, [1,2,3], "Sum does not match");
+      assert.strictEqual(called, 3, "Reducer method must be used");
+    });
+  });
 });
 
